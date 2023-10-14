da81ae7016b8d16b461bbdd5d43367cf8878a866
brian-o1 <brianmustafa01@gmail.com>
2023-10-14 15:27:45 -0400

    added custom function 'do_update_raw_data()'

--- monitor.py
+++ monitor.py
@@ -91,6 +91,11 @@ class SerialPlot:
         self.__get_serial_data(False)
         min_average = np.inf
         min_plot = None
+        # sub_plot[0] is a list of values; in this case, representing the plot variable
+        # subplot[1] is a list of values; in this case, representing the axes variable
+        # self.__data is a list of raw data of the same length as there are a number of plots
+        print("self.__data: ", self.__data)
+        print("self.__data: ", self.__data)
         for plot, ax, d in zip(sub_plot[0], sub_plot[1], self.__data):
             plot.set_data(range(self.__max_len), d)
             plot.set_color('b')
@@ -182,20 +187,27 @@ class SerialPlot:
 
 class Draw:
 
-    def __init__(self, serial_port, baud, num_of_sensors, fft_length, ser_instance=False):
+    def __init__(self, serial_port, baud, num_of_sensors, fft_length, ser_instance=False, is_debug=False):
         fft_len = fft_length
-        if not ser_instance:
-            try: 
-                print("Oops. I am in here.")
-                sp = SerialPlot("/dev/" + str(serial_port), baud, 25, num_of_sensors,
-                            fft_length)
-            except:
-                print("I am here!") 
-                sp = SerialPlot("" + str(serial_port), baud, 25, num_of_sensors,
-                            fft_length)
+
+        if not is_debug:
+            
+            if not ser_instance:
+                try: 
+                    print("Oops. I am in here.")
+                    sp = SerialPlot("/dev/" + str(serial_port), baud, 25, num_of_sensors,
+                                fft_length)
+                except:
+                    print("I am here!") 
+                    sp = SerialPlot("" + str(serial_port), baud, 25, num_of_sensors,
+                                fft_length)
+            else:
+                sp = SerialPlot("", baud, 25, num_of_sensors,
+                                fft_length, ser_instance)
         else:
-            sp = SerialPlot("", baud, 25, num_of_sensors,
-                            fft_length, ser_instance)
+            # Defining the sp variable b/c sp will not exist if there is no physical device connected
+            sp = None
+
         # define fig
         self.rawd_fig, self.rawd_ax = plt.subplots(2, 2)
         self.fft_fig, self.fft_ax = plt.subplots(2, 1)
@@ -222,7 +234,7 @@ class Draw:
         self.fft_fig.canvas.manager.set_window_title(
             'FFT len={} & Average'.format(fft_len))
         self.rawd_fig.canvas.manager.set_window_title('Raw Data')
-        anim = animation.FuncAnimation(self.rawd_fig, sp.update_raw_data,
+        anim = animation.FuncAnimation(self.rawd_fig, self.do_update_raw_data(sp, is_debug),
                                        fargs=((self.rawd_plot,
                                                self.rawd_ax.reshape(4, )),),
                                        interval=1)
@@ -231,14 +243,55 @@ class Draw:
                                         interval=1)
         plt.show()
 
-
+    def do_update_raw_data(self, sp, is_debug):
+        # Want to return sp.update_raw_data if it makes sense
+        # Otherwise, return fake raw data
+        if not is_debug:
+            raw_output_data = sp.update_raw_data
+            print("raw_output_data: ", raw_output_data)
+            print("type of raw_output_data: ", type(raw_output_data))
+            return raw_output_data
+        else:
+            return update_raw_data_for_debug
+        
 def main():
     try:
-        d = Draw(serial_port=sys.argv[1], baud=int(sys.argv[2]),
-                 fft_length=int(sys.argv[3]), num_of_sensors=4)
+        serial_port = sys.argv[1]
+        baud = int(sys.argv[2])
+        fft_length = int(sys.argv[3])
+        num_of_sensors = 4
+        is_debug = sys.argv[4]
+
+        if is_debug.lower() == "true":
+            is_debug = True
+        else:
+            is_debug = False
+
+        d = Draw(serial_port=serial_port, baud=baud,
+                 fft_length=fft_length, num_of_sensors=num_of_sensors, is_debug=is_debug)
     except KeyboardInterrupt:
         exit()
 
+# the purpose is to execute same thing with fake raw data
+def update_raw_data_for_debug(frame, sub_plot):
+        #print("sub_plot: ": sub_plot)
+        min_average = np.inf
+        min_plot = None
+        __data = [deque([0.0] * max_len) for num in range(num_of_sensors)]
+        __max_length = 256
+        # sub_plot[0] is a list of values; in this case, representing the plot variable
+        # subplot[1] is a list of values; in this case, representing the axes variable
+        # self.__data is a list of raw data of the same length as there are a number of plots
+        for plot, ax, d in zip(sub_plot[0], sub_plot[1], __data):
+            plot.set_data(range(__max_len), d)
+            plot.set_color('b')
+            ave = np.average(d)
+            ax.set_title("ave={:.2f}".format(ave))
+            if (ave < min_average):
+                min_average = ave
+                min_plot = plot
+        min_plot.set_color('r')
+        #self.__lock = False
 
 if __name__ == '__main__':
     main()
